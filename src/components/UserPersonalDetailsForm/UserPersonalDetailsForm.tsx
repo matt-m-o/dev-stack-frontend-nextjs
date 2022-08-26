731bf0ed1b1351e769c58c7f90f1213112c39c06
MatheusM8 <matheusmoura8@hotmail.com>
2022-08-26 02:32:48 -0300

    fixed user form issue (chromium browsers)

--- src/components/UserPersonalDetailsForm/UserPersonalDetailsForm.tsx
+++ src/components/UserPersonalDetailsForm/UserPersonalDetailsForm.tsx
@@ -1,50 +1,63 @@
 import { Grid, Group, TextInput, Text } from "@mantine/core";
 import { useForm } from '@mantine/form'
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 
 import useStyles  from '../../styles/Form.styles';
-import { IUserPersonalInfo } from "../../types";
+import { IUser } from "../../types";
 
+type UserFormData = {    
+    first_name: string;
+    last_name: string;
+    email: string;    
+}
 
-type Props = {
-    formData: IUserPersonalInfo | null
-    setFormData: Dispatch<SetStateAction< IUserPersonalInfo | null >>
+interface Props {
+    formData: UserFormData | null
+    setFormData: Dispatch<SetStateAction< IUser | null >>;
 }
 
 export function UserPersonalDetailsForm ({ formData, setFormData }: Props) {
 
     const { classes } = useStyles();
 
-    const form = useForm({
-        initialValues: {
-            first_name: formData ? formData.first_name : '',
-            last_name: formData ? formData.last_name : '',
-            email: formData ? formData.email : '',
-        },
-    });
 
-    function validateForm (data: IUserPersonalInfo) {
-        // simple validation
-        return (
-            data.first_name.length > 0 &&
-            data.last_name.length > 0 &&
-            data.email.length > 0
-        )        
+    const inputsInitialValues: UserFormData = {
+        first_name: formData ? formData.first_name : '',
+        last_name: formData ? formData.last_name : '',
+        email: formData ? formData.email : '',
     }
 
-    function handleFormChange(change: Partial<IUserPersonalInfo>) { 
-        const updated = { 
-            ...(formData ? formData : form.values),
-            ...change 
+    const [ formInputs, setFormInputs ] = useState <UserFormData> (inputsInitialValues);
+
+
+    function validateForm (data: IUser) {
+        // simple validation        
+        return (
+            data?.first_name?.length > 0 &&
+            data?.last_name?.length > 0 &&
+            data?.email?.length > 0
+        )        
+    }
+    
+    let _formInputValues
+    function handleFormChange(change: Partial<UserFormData>) {
+        const _formInputValues = { 
+            ...formInputs,
+            ...change
         };
 
-        validateForm(updated) ? setFormData(updated) : setFormData(null);
+        if (!_formInputValues) return;
 
-        console.log(formData);
+        setFormInputs(_formInputValues as UserFormData);
+
+        validateForm(_formInputValues) ? 
+            setFormData(_formInputValues) : setFormData(null);
+
+        //console.log(formData);
     }
 
     return (
-        <>            
+        <>
             <Group position="center">
 
                 <form>
@@ -56,7 +69,8 @@ export function UserPersonalDetailsForm ({ formData, setFormData }: Props) {
                                     <span className={classes.inputLabel}>First Name</span> 
                                 }
                                 size='sm'
-                                {...form.getInputProps('first_name')}
+                                value={formData?.first_name}
+                                //{...form.getInputProps('first_name')}
                                 onInputCapture = { ({target}) => handleFormChange({ first_name: target?.value }) }
                                 required
                             />
@@ -64,14 +78,15 @@ export function UserPersonalDetailsForm ({ formData, setFormData }: Props) {
 
                         <Grid.Col span={6}>
                             <TextInput            
-                            label= {
-                                <span className={classes.inputLabel}>Last Name</span> 
-                            }
-                            size='sm'
-                            {...form.getInputProps('last_name')}
+                                label= {
+                                    <span className={classes.inputLabel}>Last Name</span> 
+                                }
+                                size='sm'
+                                value={formData?.last_name}
+                                //{...form.getInputProps('last_name')}
 
-                            onInputCapture = { ({target}) => handleFormChange({ last_name: target?.value }) }
-                            required                  
+                                onInputCapture = { ({target}) => handleFormChange({ last_name: target?.value }) }
+                                required                  
                             />
                         </Grid.Col>
 
@@ -82,8 +97,9 @@ export function UserPersonalDetailsForm ({ formData, setFormData }: Props) {
                                 <span className={classes.inputLabel}>Email</span>
                             }
                             size='sm'
-                            type='email'                  
-                            {...form.getInputProps('email')}
+                            type='email'
+                            value={formData?.email}
+                            //{...form.getInputProps('email')}
 
                             onInputCapture = { ({target}) => handleFormChange({ email: target?.value }) }
                             required                  
--- src/styles/Form.styles.ts
+++ src/styles/Form.styles.ts
@@ -5,7 +5,7 @@ export default createStyles((theme) => ({
   title: {
     fontSize: 20,
     textAlign: 'center',
-    /* marginBottom: 40, */
+    fontWeight: 500,
   },
 
   inputLabel: {    
